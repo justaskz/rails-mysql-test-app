9e2412ad3b5719754635c58a0ad43acd9cc2ff5a
Justas Kazakauskas <justas.kazakauskas@gmail.com>
2020-06-10 00:51:43 +0300

    Add complete docker-compose solution

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,2 +1,11 @@
 require 'sidekiq/throttled'
+
 Sidekiq::Throttled.setup!
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://redis:6379' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://redis:6379' }
+end
